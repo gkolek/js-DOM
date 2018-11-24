0578c9ad5d9f7345a5af2c01d38e4a40482728fe
Mateusz Choma <chomamateusz@gmail.com>
2018-11-24 13:30:38 +0100

    CSS style

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ btn.setAttribute('disabled', '')
 btn.setAttribute('id', 'button')
 
 btn.removeAttribute('disabled')
+
+// btn.style.backgroundColor = 'red'
+// btn.style.borderRadius = '5px'
+// btn.style.border = 'none'
+btn.style.color = 'white'
+
+// cssText overwrites all styles
+btn.style.cssText = `
+  background-color: red;
+  border-radius: 5px;
+  border: none;
+`
