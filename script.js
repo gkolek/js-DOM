6f5d35b5ad46f90a7ee77dfb7664ec638cdc5cd5
Mateusz Choma <chomamateusz@gmail.com>
2018-11-24 11:39:15 +0100

    Removing elements

--- script.js
+++ script.js
@@ -1,30 +1,7 @@
-const divById = document.getElementById('first')
+const element1 = document.querySelector('#first')
 
-console.log(divById)
-console.dir(divById)
+element1.remove()
 
-const divsByClass = document.getElementsByClassName('second')
-console.log(divsByClass)
+const elements = document.querySelectorAll('.second')
 
-// we can traverse HTMLCollection by for loop
-// but it do not have forEach function
-for(let i = 0; i < divsByClass.length; i++){
-	console.log(divsByClass[i])
-}
-
-const divsByTag = document.getElementsByTagName('div')
-console.log(divsByTag) 
-
-// QS for querySelector
-
-const divByIdQS = document.querySelector('#first')
-console.log(divByIdQS)
-
-// we can traverse NodeList by for loop
-// it ALSO DO have forEach function
-// but it is NOT an array
-const divsByClassQS = document.querySelectorAll('.second')
-console.log(divsByClassQS)
-
-const divsByTagQS = document.querySelectorAll('div')
-console.log(divsByTagQS) 
+elements[0].remove()
