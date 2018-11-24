faca8ce8c85137c2eff62d489bf3754b7923bde9
Mateusz Choma <chomamateusz@gmail.com>
2018-11-24 10:43:38 +0100

    getElementsByTagName

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-const div = document.getElementById('first')
+const divById = document.getElementById('first')
 
-console.log(div)
-console.dir(div)
+console.log(divById)
+console.dir(divById)
+
+const divsByClass = document.getElementsByClassName('second')
+console.log(divsByClass)
+
+// we can traverse HTMLCollection by for loop
+// but it do not have forEach function
+for(let i = 0; i < divsByClass.length; i++){
+	console.log(divsByClass[i])
+}
+
+const divsByTag = document.getElementsByTagName('div')
+console.log(divsByTag) 
